6870fef707a6b83bd2bdbcfc57e9b5e3783151fc
Daniel Libaneo <daniel.libaneo@gmail.com>
2017-07-14 11:51:16 -0300

    Create js and Location.scss

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function initMap() {
+  const myLatlng = {lat: -23.482069, lng: -47.425131};
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: myLatlng,
+    scrollwheel: false,
+  });
+
+  const marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Fatec Sorocaba',
+    icon: 'img/pin.png'
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: '<strong>Fatec Sorocaba</strong> <br> Av. Eng. Carlos Reinaldo Mendes, 2015',
+    maxWidth: 300
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+  infowindow.open(map,marker);
+
+  map.addListener('center_changed', function() {
+    window.setTimeout(function() {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
+}
